684f5a02a6521e1b76b1947eb932d80622b2d2c2
오성인 <dhtjddls12@naver.com>
2022-10-07 16:39:32 +0900

    study: attendence check complete and pomodoro start

--- side_project/attendence_check/app.js
+++ side_project/attendence_check/app.js
@@ -5,7 +5,12 @@ let att = document.querySelector('.att');
 let ab = document.querySelector('.ab');
 let nonStu = document.querySelector('.nonStu');
 const back = document.querySelector('body');
+let reBtn = document.querySelector('.re-btn');
+let button = document.querySelectorAll('button');
 
+const darkmodeBtn = document.querySelector('.dark-toggle');
+
+const tipsBtn = document.querySelector('.tips');
 
 calBtn.addEventListener('click', window.btn = function() {
     let totalNum = student.length;
@@ -37,5 +42,37 @@ back.addEventListener('keydown', (e) =>{
     }
 });
 
+darkmodeBtn.addEventListener('click', () => {
+    if (back.classList.contains('dark-mode')) {
+        back.classList.remove('dark-mode');
+        for(i of button){
+            i.classList.remove('dark-mode');
+        }
+    }
+    else {
+        back.classList.add('dark-mode');
+        for(i of button){
+            i.classList.add('dark-mode');
+        }
+    }
+})
 
 
+tipsBtn.addEventListener('click', () => {
+    swal({
+        title: "Tab, Spacebar, Enter 키를 사용하세요!",
+    });
+})
+
+reBtn.addEventListener('click', () => {
+    total.innerText = `총원 : `;
+    att.innerText = `출석 : `;
+    ab.innerHTML = `결석 : `;
+    nonStu.innerText = `없는 사람 : `;
+    const query = 'input[name="sitted"]:checked'; 
+    const seleted = document.querySelectorAll(query);
+    for (let i = 0; i < seleted.length; i++) {
+        seleted[i].checked = false;
+        
+    }
+});
