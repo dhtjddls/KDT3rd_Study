39277f48cc8ef9409ab3c4a15fec34d6fe7e3c67
오성인 <dhtjddls12@naver.com>
2022-10-12 09:27:22 +0900

    design

--- side_project/counter/counter_index.js
+++ side_project/counter/counter_index.js
@@ -16,6 +16,9 @@ minus.addEventListener('click', () =>{
     else if(cnt > 0) {
         number.style.color = 'blue';
     }
+    else {
+        number.style.color = 'black';
+    }
 });
 
 plus.addEventListener('click', () =>{
@@ -28,11 +31,15 @@ plus.addEventListener('click', () =>{
     else if(cnt > 0) {
         number.style.color = 'blue';
     }
+    else {
+        number.style.color = 'black';
+    }
 });
 
 reset.addEventListener('click', () =>{
     cnt = 0;
     number.innerHTML = cnt;
+    number.style.color = 'black';
     // console.log(cnt);
 });
 
